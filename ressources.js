b382694100cee7b714a97df6c40a748f2d1c40d2
Sh1r0w <k4leth@gmail.com>
2021-03-29 22:42:23 +0200

    update

--- ressources.js
+++ ressources.js
@@ -1,26 +1,3 @@
-/* connection au server et récupération des données caméras */
-/*const camerasList = (callback) => {
-const request = new XMLHttpRequest();
-request.open('GET', 'http://localhost:3000/api/cameras');
-request.send();
-request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200) {
-        callback(undefined, request.responseText)
-    }else if(request.readyState ===4){
-        callback('impossible de trouver les données', undefined)
-    }
-});
-}
-
-console.log();
-camerasList((err, data)=> {
-if(err){
-    console.log(err);
-}else{
-    console.log(data);
-}
-});*/
-
 const request = new XMLHttpRequest();
 request.open('GET', 'http://localhost:3000/api/cameras');
 request.send();
@@ -30,7 +7,7 @@ request.onreadystatechange = function() {
         let r = response;
         console.log(r);
         for (let r in response) {
-            document.querySelector('#listing').innerHTML += '<div class="card"><img class="card-img-top" src="'+response[r].imageUrl+'" alt="'+response[r].name+'"><div class="card-body"><h5 class="card-title">'+response[r].name+'</h5><p class="card-text">'+response[r].description+'</p></div></div>';
+            document.querySelector('#listing').innerHTML += '<div class="col-3 p-2 m-2 card"><img class="card-img-top" src="'+response[r].imageUrl+'" alt="'+response[r].name+'"><div class="card-body"><h5 class="card-title">'+response[r].name+'</h5><p class="card-text">'+response[r].description+'</p><div class="card-price">Prix : '+response[r].price+' € TTC</div><a href="#" class="btn btn-danger p-2">Acheter</a></div></div>';
         }
     }
         
