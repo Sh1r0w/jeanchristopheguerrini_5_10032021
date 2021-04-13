8b417ea801f65e8b1ef8040286cb2ed029077102
Sh1r0w <k4leth@gmail.com>
2021-04-13 22:02:14 +0200

    update

--- ressources.js
+++ ressources.js
@@ -4,32 +4,15 @@ const recapPanier = document.querySelector('#recap');
 const panier = document.querySelector('#panier');
 const listing = document.querySelector('#listing');
 const imgLocal = localStorage.getItem('img');
-const compteur = 0;
 
 fetch(url)
     .then(function (data) {
         data.json().then(function (response) {
             for (r in response) {
-                listing.innerHTML += '<div class="col-3 p-2 m-2 card"><img class="card-img-top" src="' + response[r].imageUrl + '" alt="' + response[r].name + '"><div class="card-body"><h5 class="card-title">' + response[r].name + '</h5><p class="card-text">' + response[r].description + '</p><div class="card-price">Prix : ' + response[r].price + ' € TTC</div><a href="#" class="btn btn-danger m-2 productCard" id="' + response[r]._id + '">Acheter</a><a href="" class="btn btn-danger m-2 ' + response[r]._id + '">En Savoir Plus</a></div></div>'
-            }
-
-            const index = document.querySelectorAll(".productCard")
-
-            for (a in response) {
-                //const idProduct = response[a]._id;
-                //const addName = response[a].name;
-                //const addPrice = response[a].price;
-                console.log(response[a]._id, response[a].name, response[a].price)
-
-
+                listing.innerHTML += '<div class="col-3 p-2 m-2 card"><img class="card-img-top" src="' + response[r].imageUrl + '" alt="' + response[r].name + '"><div class="card-body"><h5 class="card-title">' + response[r].name + '</h5><p class="card-text">' + response[r].description + '</p><div class="card-price">Prix : ' + response[r].price + ' € TTC</div><a href="#" class="btn btn-danger m-2 productCard" id="' + [r] + '">Acheter</a><a href="" class="btn btn-danger m-2 ' + response[r]._id +'">En Savoir Plus</a></div></div>'        
                 for (let i = 0; i < response.length; i++) {
-                    index[i].addEventListener('click', (event => {
-                        event.preventDefault();
-                        localStorage.setItem('id' + [i], response[a]._id);
-                        localStorage.setItem('name' + [i], response[a].name);
-                        localStorage.setItem('price' + [i], response[a].price);
-                        localStorage.setItem('img' + [i], response[a].imageUrl);
-                    }));
+                    let response_json = JSON.stringify(response[i]);
+                    localStorage.setItem(+[i], response_json);
                 }
             }
         })
@@ -41,8 +24,11 @@ const retourPanier = new Promise((resolve, reject) => {
         listing.classList.add('d-none');
         listing.classList.remove('d-inline-flex');
         for (let i in localStorage) {
-            //recapPanier.innerHTML += '<div class=""><img class="card-img-top" src="'+ imgLocal.localStorage.length +'">'
-            console.log(localStorage[i]);
+            const localId = JSON.parse(localStorage.getItem(+[i]))
+            recapPanier.innerHTML += '<div class=""><img class="card-img-top col-2" src="'+ localId.imageUrl +'">'
+              console.log(localId.name);
         }
     }))
 })
+let adresseActuelle = window.location
+console.log(adresseActuelle);
