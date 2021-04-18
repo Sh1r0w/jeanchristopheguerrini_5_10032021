ed3e66882dc7b09d260529e06fce9d27f6fbe9b4
Sh1r0w <k4leth@gmail.com>
2021-04-18 18:19:03 +0200

    Update ressources.js

--- ressources.js
+++ ressources.js
@@ -5,51 +5,39 @@ const panier = document.querySelector('#panier');
 const listing = document.querySelector('#listing');
 const imgLocal = localStorage.getItem('img');
 
+
+
+// récuprération liste des caméras et affichage / enregistrement dans le local storage
+
 fetch(url)
     .then(function (data) {
         data.json().then(function (response) {
             for (r in response) {
-                listing.innerHTML += '<div class="col-3 p-2 m-2 card"><img class="card-img-top" src="' + response[r].imageUrl + '" alt="' + response[r].name + '"><div class="card-body"><h5 class="card-title">' + response[r].name + '</h5><p class="card-text">' + response[r].description + '</p><div class="card-price">Prix : ' + response[r].price + ' € TTC</div><a href="#" class="achat btn btn-danger m-2 productCard" id="' + [r] + '">Acheter</a><a href="" class="btn btn-danger m-2 ' + response[r]._id +'">En Savoir Plus</a></div></div>'        
-                const achat = document.getElementById(+[r]);
+                listing.innerHTML += '<div class="col-3 p-2 m-2 card"><img class="card-img-top" src="' + response[r].imageUrl + '" alt="' + response[r].name + '"><div class="card-body"><h5 class="card-title">' + response[r].name + '</h5><p class="card-text">' + response[r].description + '</p><div class="card-price">Prix : ' + response[r].price + ' € TTC</div><a href="#" class="achat btn btn-danger m-2 productCard" id="' + [r] + '">Acheter</a><a href="" class="btn btn-danger m-2 ' + response[r]._id + '">En Savoir Plus</a></div></div>'
+                
                 for (let i = 0; i < response.length; i++) {
                     let response_json = JSON.stringify(response[i]);
                     localStorage.setItem(+[i], response_json);
-                    
                 }
-                const lecture = JSON.parse(localStorage.getItem(+[r]));
-                console.log(lecture.name);
 
-                achat.addEventListener('click', (event => {
-                    event.preventDefault()
-                        let i = 0;
-                        for (const p in response) {                            
-                            let itemPanier = {"name":[+lecture.name]};/*[ 'name :' +response[r].name, 'image :' +response[r].imageUrl, 'prix' +response[r].price, 'quantité']*/;
-                            let itemPanier_json = JSON.stringify(itemPanier);
-                            localStorage.setItem('p'+[r], itemPanier_json)
-                            /*if(i != 0){
-                            qt = parseInt(localStorage.getItem('PanierLocal'+[i]));
-                            qt++;
-                            localStorage.setItem('PanierLocal'+[i], qt);
-                        }*/
-                        }
-                       }
-                ))
-                
-            }
+// lecture des cameras pour l'enregistrement dans le panier
+                const achat = document.getElementById(+[r]);
+                const lecture = JSON.parse(localStorage.getItem(+[r]));
+                console.log(achat);
+                let q = 1;             
+                document.querySelectorAll('.achat').forEach(item => {
+                    item.addEventListener('click', (event => {
+
+                    console.log(achat)
+                    let itemPanier = { name: lecture.name, price: lecture.price, image: lecture.imageUrl, quantité: q++ };
+                    let itemPanier_json = JSON.stringify(itemPanier);
+                    localStorage.setItem('p' + [r], itemPanier_json);
+                }
+                ))            
+        })
+    }
         })
     })
-
-/*const rajoutPanier = new Promise((resolve, reject) => {
-    achat.addEventListener('click', (event => {
-        event.preventDefault();
-        for (let r in localStorage('panierLocal'))
-            for(let i = 0; i < r; i++) {
-                let panier_json= JSON.stringify(localStorage.setItem(+[r]+[i]));
-            }
-    }))
-})*/
-
-
 
 const retourPanier = new Promise((resolve, reject) => {
     panier.addEventListener('click', (event => {
@@ -59,8 +47,8 @@ const retourPanier = new Promise((resolve, reject) => {
         for (let i in localStorage) {
             const localId = JSON.parse(localStorage.getItem(+[i]))
             recapPanier.innerHTML += '<div class=""><img class="card-img-top col-2" src="' + localId.imageUrl + '"><h5 class="card-title">' + localId.name + '</h5>'
-              console.log(localId.name);
-              console.log(localId);
+            console.log(localId.name);
+            console.log(localId);
         }
     }))
 })
